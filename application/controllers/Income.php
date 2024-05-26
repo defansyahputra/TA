57854587be99fdd267a1c116992d32d884cd9c97
defan <defansyahputra@gmail.com>
2024-05-26 17:53:41 +0700

    26052024-defan

---
+++ application/controllers/Income.php
@@ -0,0 +1,73 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Income extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->library('tank_auth');
+        $this->load->helper('rupiah');
+
+
+        if (!$this->tank_auth->is_logged_in()) {
+            redirect('/auth/login/');
+        } else {
+            $this->data['dataUser'] = $this->session->userdata('data_ldap');
+
+            $this->data['user_id'] = $this->tank_auth->get_user_id();
+            $this->data['username'] = $this->tank_auth->get_username();
+            $this->data['email'] = $this->tank_auth->get_email();
+
+            $profile = $this->tank_auth->get_user_profile($this->data['user_id']);
+
+            $this->data['profile_name'] = $profile['name'];
+            $this->data['profile_foto'] = $profile['foto'];
+
+            foreach ($this->tank_auth->get_roles($this->data['user_id']) as $val) {
+                $this->data['role_id'] = $val['role_id'];
+                $this->data['role'] = $val['role'];
+                $this->data['full_name_role'] = $val['full'];
+            }
+
+            $this->data['link_active'] = 'Income';
+
+            //buat permission
+            if (!$this->tank_auth->permit($this->data['link_active'])) {
+                redirect('Income');
+            }
+
+            $this->load->model("Showmenu_model");
+            $this->data['ShowMenu'] = $this->Showmenu_model->getShowMenu();
+
+            $OpenShowMenu = $this->Showmenu_model->getOpenShowMenu($this->data);
+
+            $this->data['openMenu'] = $this->Showmenu_model->getDataOpenMenu($OpenShowMenu->id_menu_parent);
+
+            $this->load->model("Menu_model");
+            // $this->load->model("Income_model");
+        }
+    }
+
+    public function index()
+    {
+
+        $this->data['title'] = 'Income';
+
+        $this->data['breadcrumbs'] = [];
+
+        $this->data['breadcrumbs'][] = [
+            'active' => FALSE,
+            'text' => 'Income',
+            'class' => 'breadcrumb-item pe-3 text-gray-400',
+            'href' => site_url('Income')
+        ];
+
+        $this->load->view('component/header', $this->data);
+        $this->load->view('component/sidebar', $this->data);
+        $this->load->view('component/navbar', $this->data);
+        $this->load->view('Income/view', $this->data);
+        $this->load->view('component/footer');
+    }
+}
